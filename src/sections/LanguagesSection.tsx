a48c2c34dac216bc1eddf6140afd49f31bd697a6
Łukasz Anuszkiewicz <odysey84@o2.pl>
2022-02-23 13:47:47 +0100

    Change language skills section for more standarized levels (A1-C2 CERF)

--- src/sections/LanguagesSection.tsx
+++ src/sections/LanguagesSection.tsx
@@ -4,30 +4,22 @@ import { Skill } from '../components/Skill/Skill'
 const languages = [
   {
     name: 'English',
-    value: 2
+    value: 3
   },
   {
     name: 'Polish',
-    value: 5
+    value: 6
   }
 ]
-
-const signs = {
-  solid: '★',
-  hollow: '☆'
-}
 
 export const LanguagesSection = (): JSX.Element => {
   return (
     <>
       <h3 className={'section-head col'}>LANGUAGES</h3>
       {languages.map(({ name, value }) => {
-        const filled = Array(5).fill(signs.solid, 0, 5) as string[]
-        filled.fill(signs.hollow, value, 5)
-
         return (
           <div key={name} className="col section-content">
-            <Skill name={name} value={value} />
+            <Skill name={name} value={value} range={6} />
           </div>
         )
       })}
