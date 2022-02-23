8d1186cddd2253b5678eafc77ad99a4af1abd8b6
Łukasz Anuszkiewicz <odysey84@o2.pl>
2022-02-23 15:56:17 +0100

    Short form of month names in Experience sec

--- src/sections/ExperienceSection.tsx
+++ src/sections/ExperienceSection.tsx
@@ -3,31 +3,31 @@ import { Experience } from '../components/Experience/Experience'
 
 const expiriences = [
   {
-    date: 'February 2019 – April 2020',
+    date: 'Feb 2019 – Apr 2020',
     func: 'Junior Frontend Developer',
     company: 'ORBA Sp.z o.o.',
     place: 'Warsaw'
   },
   {
-    date: 'October 2017 – December 2017',
+    date: 'Oct 2017 – Dec 2017',
     func: 'Frontend developer',
     company: 'Perspektywy Press',
     place: 'Warsaw'
   },
   {
-    date: 'September 2016 – September 2017',
+    date: 'Sep 2016 – Sep 2017',
     func: 'Junior Frontend Developer',
     company: 'ROOTSHER',
     place: 'Warsaw'
   },
   {
-    date: 'April 2005 – March 2012',
+    date: 'Apr 2005 – Mar 2012',
     func: 'Administrator sieci',
     company: 'Mlekomat Sp.Jawna',
     place: 'Karczew'
   },
   {
-    date: 'December 2008 – December 2009',
+    date: 'Dec 2008 – Dec 2009',
     func: 'Helpdesk',
     company: 'eFuzja',
     place: 'Warsaw'
