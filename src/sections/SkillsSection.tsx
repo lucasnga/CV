2d885adb28622511d6aac2a9e2685422bb9dea74
Łukasz Anuszkiewicz <odysey84@o2.pl>
2022-02-23 13:43:45 +0100

    Change section layout to three colunms. Change/Shorted skill list/names

--- src/sections/SkillsSection.tsx
+++ src/sections/SkillsSection.tsx
@@ -2,30 +2,30 @@ import React from 'react'
 import { Skill } from '../components/Skill/Skill'
 
 const skills = [
-  { value: 3, name: 'HTML / CSS / SCSS (RWD / Compatibility)' },
+  { value: 3, name: 'HTML / CSS / SCSS' },
   { value: 3, name: 'Javascript / TypeScript' },
   { value: 3, name: 'Git' },
-  { value: 3, name: 'React / redux / react-router' },
-  { value: 2, name: 'VueJS / vuex/ vue-router' },
+  { value: 3, name: 'React (redux / router)' },
+  { value: 2, name: 'VueJS (vuex / router)' },
   { value: 4, name: 'REST API / axios / fetch' },
   {
     value: 2,
-    name: 'Testing (Jest / Enzyme, Cypress / StoryBook)'
+    name: 'Testing (Jest / Cypress)'
   },
-  { value: 2, name: 'Scrum / Kanban (Jira / Trello / Assembla)' },
-  { value: 4, name: 'Linux (Fedora / Debian / Gentoo)' },
-  { value: 3, name: 'Docker / Compose / Vagant' },
+  { value: 2, name: 'Scrum / Kanban (Jira / Assembla)' },
+  { value: 4, name: 'Linux (Fedora / Debian)' },
+  { value: 3, name: 'Docker / Vagant' },
   { value: 3, name: 'yarn / npm / parcel' },
   { value: 2, name: 'Python / Bash (Scripting) ' },
   { value: 3, name: 'MySQL / PostreSQL' },
-  { value: 2, name: 'MongoDB / Redis / Cassanda' },
+  { value: 2, name: 'MongoDB / Redis' },
   { value: 4, name: 'babel / webpack / gulp' },
-  { value: 3, name: 'underscore / lodash / ramda' },
+  { value: 3, name: 'lodash / ramda' },
   { value: 1, name: 'Design patterns' },
-  { value: 2, name: 'Node JS / NestJS / express / restify' },
+  { value: 2, name: 'Node JS / NestJS / express' },
   { value: 4, name: 'Bootstrap / Material-UI' },
   { value: 2, name: 'Elasticsearch' },
-  { value: 1, name: 'React Native' },
+  { value: 1, name: 'React Native / Expo' },
   { value: 2, name: 'Magento 2 - Frontend (KnockoutJS / jQuery)' }
 ]
 
@@ -33,8 +33,7 @@ export const SkillsSection = (): JSX.Element => {
   return (
     <>
       <h3 className={'section-head'}>SKILLS</h3>
-      <div className="col-4 skills-col" />
-      <div className="col skills-col" style={{ columns: 2 }}>
+      <div className="col skills-col" style={{ columns: 3 }}>
         {skills.map((skill) => (
           <Skill key={skill.name} name={skill.name} value={skill.value} />
         ))}
