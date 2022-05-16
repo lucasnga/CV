67e67a8fafa0f8e60ea9cb3922363bc2a900c346
Łukasz Anuszkiewicz <odysey84@o2.pl>
2022-05-16 08:56:02 +0200

    Simplify level generation

--- src/components/Skill/Skill.tsx
+++ src/components/Skill/Skill.tsx
@@ -14,17 +14,15 @@ export function Skill({
   value: number
   range?: number
 }): JSX.Element {
-  const filled = Array(range)
-    .fill(signs.solid, 0, range)
-    .fill(signs.hollow, value, range) as string[]
-
   return (
     <div className={'row skill pb-1'}>
       <div className={'col'}>
         <span className={'skill-name'}>{name}</span>
       </div>
       <div className={'col-3 p-0'}>
-        <span className={'skill-score'}>{filled.join('').toString()}</span>
+        <span className={'skill-score'}>
+          {Array(range).fill(signs.solid).fill(signs.hollow, value).join('')}
+        </span>
       </div>
     </div>
   )
